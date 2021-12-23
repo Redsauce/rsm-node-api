db43db6689d646c788bd29707799ddf911776476
Kenan Rhoton <krhoton@redsauce.net>
2021-12-23 11:39:24 +0100

    Restored old Exception messages for node requests

--- request.js
+++ request.js
@@ -1,7 +1,19 @@
 const {isNode} = require("browser-or-node");
 
 if (isNode) {
-  module.exports.fetch = require("node-fetch");
+  f = require("node-fetch");
+  module.exports.fetch = async function(...args) {
+    try {
+      const result = await f(...args);
+      return result;
+    } catch (e) {
+      if (e instanceof f.FetchError) {
+        throw new Error(e.message.split("reason: ")[1]);
+      } else {
+        throw e;
+      }
+    }
+  }
 } else {
   module.exports.fetch = fetch;
 }
